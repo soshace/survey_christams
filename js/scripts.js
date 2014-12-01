4c0431171c8fa466dac5f726bc31f1485d9375f4
branikita <nikita-bragin@mail.ru>
2014-12-01 16:52:26 +0300

    First step completed

--- js/scripts.js
+++ js/scripts.js
@@ -66,22 +66,22 @@ $(function () {
         document.title = originalTitle;
     }
 
-    $('.js-player').jPlayer({
-        ready: function () {
-            $(this).jPlayer('setMedia', {
-                mp3: '/audio/uk.mp3'
-            }).jPlayer('play');
-        },
-        swfPath: '/js/vendors/jplayer',
-        supplied: 'mp3',
-        cssSelectorAncestor: '',
-        cssSelector: {
-            play: '.js-play',
-            pause: '.js-pause',
-            playBar: '.js-play-bar',
-            noSolution: '.js-no-solution'
-        }
-    });
+    //$('.js-player').jPlayer({
+    //    ready: function () {
+    //        $(this).jPlayer('setMedia', {
+    //            mp3: '/audio/uk.mp3'
+    //        }).jPlayer('play');
+    //    },
+    //    swfPath: '/js/vendors/jplayer',
+    //    supplied: 'mp3',
+    //    cssSelectorAncestor: '',
+    //    cssSelector: {
+    //        play: '.js-play',
+    //        pause: '.js-pause',
+    //        playBar: '.js-play-bar',
+    //        noSolution: '.js-no-solution'
+    //    }
+    //});
 
     $('.js-next').on('click', function (event) {
         event.preventDefault();
